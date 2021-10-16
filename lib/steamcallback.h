56624fcf0c5a547491310b068d2e5ee96b42f5df
Chase <29675446+theace0296@users.noreply.github.com>
2021-10-16 12:39:44 -0400

    Fix formating of generated cpp files

--- lib/steamcallback.h
+++ lib/steamcallback.h
@@ -1,31 +1,30 @@
 
-  #include <stdio.h>
-  #include "steam_api.h"
-  #include "isteamgameserver.h"
-  #include "isteamgameserverstats.h"
-  namespace CCallBacks {
-  
-    class CGetVideoURLResult: CCallback<CGetVideoURLResult, GetVideoURLResult_t, false> {
-      public:
-        CGetVideoURLResult();
-        bool GetIsCompleted();
-        GetVideoURLResult_t GetResult();
-      private:
-        void OnGetVideoURLResult(GetVideoURLResult_t* result);
-        GetVideoURLResult_t m_result;
-        bool m_isComplete;
-    };
-  
-    class CGetOPFSettingsResult: CCallback<CGetOPFSettingsResult, GetOPFSettingsResult_t, false> {
-      public:
-        CGetOPFSettingsResult();
-        bool GetIsCompleted();
-        GetOPFSettingsResult_t GetResult();
-      private:
-        void OnGetOPFSettingsResult(GetOPFSettingsResult_t* result);
-        GetOPFSettingsResult_t m_result;
-        bool m_isComplete;
-    };
-  
+#include <stdio.h>
+#include "steam_api.h"
+#include "isteamgameserver.h"
+#include "isteamgameserverstats.h"
+namespace CCallBacks {
+
+  class CGetVideoURLResult: CCallback<CGetVideoURLResult, GetVideoURLResult_t, false> {
+    public:
+      CGetVideoURLResult();
+      bool GetIsCompleted();
+      GetVideoURLResult_t GetResult();
+    private:
+      void OnGetVideoURLResult(GetVideoURLResult_t* result);
+      GetVideoURLResult_t m_result;
+      bool m_isComplete;
   };
-  
+
+  class CGetOPFSettingsResult: CCallback<CGetOPFSettingsResult, GetOPFSettingsResult_t, false> {
+    public:
+      CGetOPFSettingsResult();
+      bool GetIsCompleted();
+      GetOPFSettingsResult_t GetResult();
+    private:
+      void OnGetOPFSettingsResult(GetOPFSettingsResult_t* result);
+      GetOPFSettingsResult_t m_result;
+      bool m_isComplete;
+  };
+
+};
