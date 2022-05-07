0cff07de7a90732313a5df5c27cd81a709fdaca1
Chase <29675446+theace0296@users.noreply.github.com>
2022-05-07 09:18:46 -0400

    update test

--- index.test.js
+++ index.test.js
@@ -2,27 +2,35 @@
 const Assert = require('assert');
 const steamworks = require('.');
 const SteamWorks = new steamworks();
-const {
-  SteamAPI,
-  Constants,
-  Enums,
-  Structs,
-  Uncategorized,
-} = SteamWorks;
+const { SteamAPI, Constants, Enums, Structs, Uncategorized } = SteamWorks;
 
 (async () => {
   if (SteamAPI.IsSteamRunning()) {
     console.log('Steamworks API Initialized!');
 
-    const remoteStorageSubscribedFilesResult = await SteamWorks.SteamRemoteStorage.EnumerateUserSubscribedFiles(0);
-    console.log(`Published files result:\n${JSON.stringify(remoteStorageSubscribedFilesResult)}`);
+    const remoteStorageSubscribedFilesResult =
+      await SteamWorks.SteamRemoteStorage.EnumerateUserSubscribedFiles(0);
+    Assert.deepStrictEqual(Object.keys(remoteStorageSubscribedFilesResult), [
+      'm_rgRTimeSubscribed',
+      'm_rgPublishedFileId',
+      'm_nTotalResultCount',
+      'm_nResultsReturned',
+      'm_eResult',
+    ]);
 
     const numSubscribedFiles = SteamWorks.SteamUGC.GetNumSubscribedItems();
+
+    Assert.strictEqual(typeof numSubscribedFiles, 'number');
+
     const subscribedFiles = Array.from(Array(numSubscribedFiles), (_, i) => i);
-    const subscribedFilesResult = SteamWorks.SteamUGC.GetSubscribedItems(subscribedFiles, numSubscribedFiles);
-    console.log(`Subscribed files result:\n${JSON.stringify(subscribedFilesResult)}`);
-    console.log(`Number of Subscribed files:\n${numSubscribedFiles}`);
-    console.log(`Subscribed files:\n${JSON.stringify(subscribedFiles)}`);
+    const subscribedFilesResult = SteamWorks.SteamUGC.GetSubscribedItems(
+      subscribedFiles,
+      numSubscribedFiles,
+    );
+
+    Assert.strictEqual(subscribedFilesResult, numSubscribedFiles);
+    Assert.strictEqual(Array.isArray(subscribedFiles), true);
+    Assert.strictEqual(subscribedFiles.length, subscribedFilesResult);
 
     SteamWorks.Shutdown();
     console.log('PASSED');
